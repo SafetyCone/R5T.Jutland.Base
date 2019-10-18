151ce833dab6c0963cdb9afe8151b57012237af7
MinexAutomation <minex.automation@gmail.com>
2019-10-18 14:03:35 -0700

    Added JSON serialization operator interface.

--- source/R5T.Jutland.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Jutland.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Jutland.Base/Code/Interfaces/IJsonSerializationOperator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Jutland
+{
+    public interface IJsonSerializationOperator
+    {
+        void Serialize<T>(string jsonFilePath, T value);
+        T Deserialize<T>(string jsonFilePath);
+    }
+}
